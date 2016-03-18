75ca79a466234c4accc9d494ced591b06895cb27
Simon Rozman <simon@rozman.si>
2016-03-18 12:49:50 +0100

    Keys and configuration for Updater module added

---
+++ include/UpdaterCfg.h
@@ -0,0 +1,21 @@
+//
+// Update log file
+//
+#define UPDATER_LOG_FILE    "ZRCola-Update.log"
+
+
+//
+// Update server(s)
+// Zero delimited list of update servers
+//
+#define UPDATER_HTTP_SERVER "zrcola.zrc-sazu.si\0zrcola-2.amebis.si\0"
+
+//
+// Update server port
+//
+#define UPDATER_HTTP_PORT   80
+
+//
+// Update server catalog location
+//
+#define UPDATER_HTTP_PATH   "/UpdateRepo/Catalog.xml"
