301f2180989ce0f9c19ce09a5a9d2e73c76e6376
Simon Rozman <simon@rozman.si>
2016-03-25 13:15:54 +0100

    Updater integration continues

--- include/UpdaterCfg.h
+++ include/UpdaterCfg.h
@@ -1,21 +1,42 @@
+#include "zrcola.h"
+
 //
 // Update log file
 //
-#define UPDATER_LOG_FILE    "ZRCola-Update.log"
+#define UPDATER_LOG_FILE            "ZRCola-Update.log"
 
 
 //
 // Update server(s)
 // Zero delimited list of update servers
 //
-#define UPDATER_HTTP_SERVER "zrcola.zrc-sazu.si\0zrcola-2.amebis.si\0"
+#define UPDATER_HTTP_SERVER         "zrcola.zrc-sazu.si\0zrcola-2.amebis.si\0"
 
 //
 // Update server port
 //
-#define UPDATER_HTTP_PORT   80
+#define UPDATER_HTTP_PORT           80
 
 //
 // Update server catalog location
 //
-#define UPDATER_HTTP_PATH   "/UpdateRepo/Catalog-1.xml"
+#define UPDATER_HTTP_PATH           "/UpdateRepo/Catalog-1.xml"
+
+
+//
+// The product vendor and application name for configuration keeping.
+//
+#define UPDATER_CFG_VENDOR          "Amebis"
+#define UPDATER_CFG_APPLICATION     "ZRCola"
+
+
+//
+// Product version as a single DWORD
+// Note: Used for version comparison within C/C++ code.
+//
+#define UPDATER_PRODUCT_VERSION     ZRCOLA_VERSION
+
+//
+// Human readable product version and build year for UI
+//
+#define UPDATER_PRODUCT_VERSION_STR ZRCOLA_VERSION_STR
