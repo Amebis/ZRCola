040109c36f112e4e3168d9dae936bd4b93ceae0b
Simon Rozman <simon@rozman.si>
2016-06-14 10:15:43 +0200

    Version set to 2.0-beta5

--- include/zrcola.h
+++ include/zrcola.h
@@ -23,7 +23,7 @@
 // Product version as a single DWORD
 // Note: Used for version comparison within C/C++ code.
 //
-#define ZRCOLA_VERSION          0x01ff0c00
+#define ZRCOLA_VERSION          0x01ff0d00
 
 //
 // Product version by components
@@ -33,26 +33,26 @@
 //
 #define ZRCOLA_VERSION_MAJ      1
 #define ZRCOLA_VERSION_MIN      255
-#define ZRCOLA_VERSION_REV      12
+#define ZRCOLA_VERSION_REV      13
 #define ZRCOLA_VERSION_BUILD    0
 
 //
 // Human readable product version and build year for UI
 //
-#define ZRCOLA_VERSION_STR      "2.0-beta4"
+#define ZRCOLA_VERSION_STR      "2.0-beta5"
 #define ZRCOLA_BUILD_YEAR_STR   "2016"
 
 //
 // Numerical version presentation for ProductVersion propery in
 // MSI packages (syntax: N.N[.N[.N]])
 //
-#define ZRCOLA_VERSION_INST     "1.255.12"
+#define ZRCOLA_VERSION_INST     "1.255.13"
 
 //
 // The product code for ProductCode property in MSI packages
 // Replace with new on every version change, regardless how minor it is.
 //
-#define ZRCOLA_VERSION_GUID     "{EAC8769F-1036-4565-828E-06B3220F1EF7}"
+#define ZRCOLA_VERSION_GUID     "{E3BC491A-D30B-47E6-9C91-7D2034E21024}"
 
 //
 // The product vendor and application name for configuration keeping.
