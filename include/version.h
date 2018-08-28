f4de83bf063b762ad3d9ee2b077bd8d6f164b301
Simon Rozman <simon@rozman.si>
2018-08-28 08:55:21 +0200

    Preset version to 2.0.3

--- include/version.h
+++ include/version.h
@@ -23,7 +23,7 @@
 // Product version as a single DWORD
 // Note: Used for version comparison within C/C++ code.
 //
-#define PRODUCT_VERSION          0x02000200
+#define PRODUCT_VERSION          0x02000300
 
 //
 // Product version by components
@@ -33,26 +33,26 @@
 //
 #define PRODUCT_VERSION_MAJ      2
 #define PRODUCT_VERSION_MIN      0
-#define PRODUCT_VERSION_REV      2
+#define PRODUCT_VERSION_REV      3
 #define PRODUCT_VERSION_BUILD    0
 
 //
 // Human readable product version and build year for UI
 //
-#define PRODUCT_VERSION_STR      "2.0.2"
+#define PRODUCT_VERSION_STR      "2.0.3"
 #define PRODUCT_BUILD_YEAR_STR   "2018"
 
 //
 // Numerical version presentation for ProductVersion propery in
 // MSI packages (syntax: N.N[.N[.N]])
 //
-#define PRODUCT_VERSION_INST     "2.0.2"
+#define PRODUCT_VERSION_INST     "2.0.3"
 
 //
 // The product code for ProductCode property in MSI packages
 // Replace with new on every version change, regardless how minor it is.
 //
-#define PRODUCT_VERSION_GUID     "{FC95E15A-2AC6-4FA0-9E26-3E5574DF2998}"
+#define PRODUCT_VERSION_GUID     "{D0E45F46-DC49-4148-BC0D-1A6A12A41129}"
 
 //
 // The product vendor and application name for configuration keeping.
