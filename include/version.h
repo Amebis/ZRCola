1feebdbbf8b44c22d4014406dc3206b2dfbde80b
Simon Rozman <simon@rozman.si>
2018-09-25 12:02:39 +0200

    Pre-set version to 2.1-beta7

--- include/version.h
+++ include/version.h
@@ -33,26 +33,26 @@
 //
 #define PRODUCT_VERSION_MAJ      2
 #define PRODUCT_VERSION_MIN      0
-#define PRODUCT_VERSION_REV      6
+#define PRODUCT_VERSION_REV      7
 #define PRODUCT_VERSION_BUILD    0
 
 //
 // Human readable product version and build year for UI
 //
-#define PRODUCT_VERSION_STR      "2.1-beta6"
+#define PRODUCT_VERSION_STR      "2.1-beta7"
 #define PRODUCT_BUILD_YEAR_STR   "2018"
 
 //
 // Numerical version presentation for ProductVersion propery in
 // MSI packages (syntax: N.N[.N[.N]])
 //
-#define PRODUCT_VERSION_INST     "2.0.6"
+#define PRODUCT_VERSION_INST     "2.0.7"
 
 //
 // The product code for ProductCode property in MSI packages
 // Replace with new on every version change, regardless how minor it is.
 //
-#define PRODUCT_VERSION_GUID     "{2881867D-6D2E-41BA-AD35-0B7275B7DEF1}"
+#define PRODUCT_VERSION_GUID     "{01B6DD2A-0481-4895-A7FB-720392B97182}"
 
 //
 // The product vendor and application name for configuration keeping.
