039dae11bc47f31000f2ebbac0798cde4c8fe0ec
Simon Rozman <simon@rozman.si>
2018-10-09 14:00:02 +0200

    Set version to 2.1

--- include/version.h
+++ include/version.h
@@ -23,7 +23,7 @@
 // Product version as a single DWORD
 // Note: Used for version comparison within C/C++ code.
 //
-#define PRODUCT_VERSION          0x02000600
+#define PRODUCT_VERSION          0x02010000
 
 //
 // Product version by components
@@ -32,27 +32,27 @@
 // separately.
 //
 #define PRODUCT_VERSION_MAJ      2
-#define PRODUCT_VERSION_MIN      0
-#define PRODUCT_VERSION_REV      7
+#define PRODUCT_VERSION_MIN      1
+#define PRODUCT_VERSION_REV      0
 #define PRODUCT_VERSION_BUILD    0
 
 //
 // Human readable product version and build year for UI
 //
-#define PRODUCT_VERSION_STR      "2.1-beta7"
+#define PRODUCT_VERSION_STR      "2.1"
 #define PRODUCT_BUILD_YEAR_STR   "2018"
 
 //
 // Numerical version presentation for ProductVersion propery in
 // MSI packages (syntax: N.N[.N[.N]])
 //
-#define PRODUCT_VERSION_INST     "2.0.7"
+#define PRODUCT_VERSION_INST     "2.1"
 
 //
 // The product code for ProductCode property in MSI packages
 // Replace with new on every version change, regardless how minor it is.
 //
-#define PRODUCT_VERSION_GUID     "{01B6DD2A-0481-4895-A7FB-720392B97182}"
+#define PRODUCT_VERSION_GUID     "{001F650E-21F6-4E38-A0D5-9B86C5A0BC6E}"
 
 //
 // The product vendor and application name for configuration keeping.
