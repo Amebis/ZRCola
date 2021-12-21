e07ce00938abf7413fba210dd7d08de88faa069f
Simon Rozman <simon@rozman.si>
2021-12-21 14:22:35 +0100

    Set version to 2.5.5
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- include/version.h
+++ include/version.h
@@ -9,7 +9,7 @@
 // Product version as a single DWORD
 // Note: Used for version comparison within C/C++ code.
 //
-#define PRODUCT_VERSION          0x02050400
+#define PRODUCT_VERSION          0x02050500
 
 //
 // Product version by components
@@ -19,26 +19,26 @@
 //
 #define PRODUCT_VERSION_MAJ      2
 #define PRODUCT_VERSION_MIN      5
-#define PRODUCT_VERSION_REV      4
+#define PRODUCT_VERSION_REV      5
 #define PRODUCT_VERSION_BUILD    0
 
 //
 // Human readable product version and build year for UI
 //
-#define PRODUCT_VERSION_STR      "2.5.4"
+#define PRODUCT_VERSION_STR      "2.5.5"
 #define PRODUCT_BUILD_YEAR_STR   "2021"
 
 //
 // Numerical version presentation for ProductVersion propery in
 // MSI packages (syntax: N.N[.N[.N]])
 //
-#define PRODUCT_VERSION_INST     "2.5.4"
+#define PRODUCT_VERSION_INST     "2.5.5"
 
 //
 // The product code for ProductCode property in MSI packages
 // Replace with new on every version change, regardless how minor it is.
 //
-#define PRODUCT_VERSION_GUID     "{EBF841F9-2BFB-444C-AC5E-874586350EFD}"
+#define PRODUCT_VERSION_GUID     "{109805EF-6EB5-4F43-A05A-82B3862E16B5}"
 
 //
 // The product vendor and application name for configuration keeping.
