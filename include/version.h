6a8cd1ec8013f1b0352d75b15eae1328c4bf1172
Simon Rozman <simon@rozman.si>
2024-04-25 14:51:30 +0200

    Preset version to 2.7.1
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- include/version.h
+++ include/version.h
@@ -9,7 +9,7 @@
 // Product version as a single DWORD
 // Note: Used for version comparison within C/C++ code.
 //
-#define PRODUCT_VERSION          0x02070000
+#define PRODUCT_VERSION          0x02070100
 
 //
 // Product version by components
@@ -19,26 +19,26 @@
 //
 #define PRODUCT_VERSION_MAJ      2
 #define PRODUCT_VERSION_MIN      7
-#define PRODUCT_VERSION_REV      0
+#define PRODUCT_VERSION_REV      1
 #define PRODUCT_VERSION_BUILD    0
 
 //
 // Human readable product version and build year for UI
 //
-#define PRODUCT_VERSION_STR      "2.7"
-#define PRODUCT_BUILD_YEAR_STR   "2023"
+#define PRODUCT_VERSION_STR      "2.7.1"
+#define PRODUCT_BUILD_YEAR_STR   "2024"
 
 //
 // Numerical version presentation for ProductVersion propery in
 // MSI packages (syntax: N.N[.N[.N]])
 //
-#define PRODUCT_VERSION_INST     "2.7"
+#define PRODUCT_VERSION_INST     "2.7.1"
 
 //
 // The product code for ProductCode property in MSI packages
 // Replace with new on every version change, regardless how minor it is.
 //
-#define PRODUCT_VERSION_GUID     "{BD675AE4-7800-412A-9F89-1C213B722B16}"
+#define PRODUCT_VERSION_GUID     "{9423BEC3-3159-4130-8C3E-48D5DE24D48A}"
 
 //
 // The product vendor and application name for configuration keeping.
